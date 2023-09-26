09df101cac0e0b4f493b8bfc81f96fb1a1bb2aa7
Ingo Oppermann <ingo@datarhei.com>
2023-09-26 17:22:39 +0200

    Allow stdin/stdout redirect in REPL

--- cmd/repl.go
+++ cmd/repl.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"strings"
@@ -33,6 +34,12 @@ var replCmd = &cobra.Command{
 			f.Close()
 		}
 
+		stdout := os.Stdout
+		stdin := os.Stdin
+		var redirectReader, redirectWriter *os.File
+		redirect := ""
+		redirectArgs := []string{}
+
 		for {
 			selected := viper.GetString("cores.selected")
 			if command, err := line.Prompt(fmt.Sprintf("%s> ", selected)); err == nil {
@@ -50,10 +57,75 @@ var replCmd = &cobra.Command{
 					continue
 				}
 
+				for i, arg := range args {
+					if arg == ">" {
+						redirect = "stdout"
+						args, redirectArgs = args[:i], args[i:]
+						break
+					} else if arg == "<" {
+						redirect = "stdin"
+						args, redirectArgs = args[:i], args[i:]
+						break
+					}
+				}
+
+				if len(redirect) != 0 && len(redirectArgs) != 2 {
+					fmt.Printf("Redirect required exactly one argument\n")
+					continue
+				}
+
+				if redirect == "stdout" {
+					file, err := os.Create(redirectArgs[1])
+					if err != nil {
+						fmt.Printf("Error creating output file: %s\n", err.Error())
+						continue
+					}
+
+					redirectReader, redirectWriter, err = os.Pipe()
+					if err != nil {
+						fmt.Printf("Error redirecting output: %s\n", err.Error())
+						file.Close()
+					}
+					os.Stdout = redirectWriter
+
+					go func(file *os.File) {
+						io.Copy(file, redirectReader)
+						file.Close()
+						redirectReader.Close()
+					}(file)
+				} else if redirect == "stdin" {
+					file, err := os.Open(redirectArgs[1])
+					if err != nil {
+						fmt.Printf("Error opening input file: %s\n", err.Error())
+						continue
+					}
+
+					redirectReader, redirectWriter, err = os.Pipe()
+					if err != nil {
+						fmt.Printf("Error redirecting input: %s\n", err.Error())
+						file.Close()
+					}
+					os.Stdin = redirectReader
+
+					go func(file *os.File) {
+						io.Copy(redirectWriter, file)
+						file.Close()
+						redirectWriter.Close()
+					}(file)
+				}
+
 				line.AppendHistory(command)
 
 				rootCmd.SetArgs(args)
 				rootCmd.Execute()
+
+				if redirect == "stdout" {
+					redirectWriter.Close()
+					os.Stdout = stdout
+				} else if redirect == "stdin" {
+					redirectReader.Close()
+					os.Stdin = stdin
+				}
 			} else {
 				fmt.Printf("Error reading line: %s\n", err.Error())
 			}
