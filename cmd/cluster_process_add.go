a2989b594ab01d6394f4c5a8016e000fb89b32bc
Ingo Oppermann <ingo@datarhei.com>
2023-09-27 10:19:57 +0200

    Allow to create process from template

--- cmd/cluster_process_add.go
+++ cmd/cluster_process_add.go
@@ -11,29 +11,70 @@ import (
 )
 
 var clusterProcessAddCmd = &cobra.Command{
-	Use:   "add",
+	Use:   "add [processid]?",
 	Short: "Add a process",
 	Long:  "Add a process to the cluster.",
+	Args:  cobra.RangeArgs(0, 1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		fromFile, _ := cmd.Flags().GetString("from-file")
-		if len(fromFile) == 0 {
-			return fmt.Errorf("no process configuration file provided")
+		processid := ""
+		if len(args) == 1 {
+			processid = args[0]
 		}
 
-		reader := os.Stdin
+		var data []byte
+		var err error
 
-		if fromFile != "-" {
-			file, err := os.Open(fromFile)
+		if len(processid) == 0 {
+			fromFile, _ := cmd.Flags().GetString("from-file")
+			if len(fromFile) == 0 {
+				return fmt.Errorf("no process configuration file provided")
+			}
+
+			reader := os.Stdin
+
+			if fromFile != "-" {
+				file, err := os.Open(fromFile)
+				if err != nil {
+					return err
+				}
+
+				reader = file
+			}
+
+			data, err = io.ReadAll(reader)
+			if err != nil {
+				return err
+			}
+		} else {
+			user := api.ProcessConfig{
+				ID:   processid,
+				Type: "ffmpeg",
+				Input: []api.ProcessConfigIO{
+					{
+						Options: []string{},
+						Cleanup: []api.ProcessConfigIOCleanup{{}},
+					},
+				},
+				Output: []api.ProcessConfigIO{
+					{
+						Options: []string{},
+						Cleanup: []api.ProcessConfigIOCleanup{{}},
+					},
+				},
+				Options:     []string{},
+				LogPatterns: []string{},
+				Metadata:    map[string]interface{}{},
+			}
+
+			data, err = json.MarshalIndent(user, "", "   ")
 			if err != nil {
 				return err
 			}
 
-			reader = file
-		}
-
-		data, err := io.ReadAll(reader)
-		if err != nil {
-			return err
+			data, _, err = editData(data)
+			if err != nil {
+				return err
+			}
 		}
 
 		config := api.ProcessConfig{}
